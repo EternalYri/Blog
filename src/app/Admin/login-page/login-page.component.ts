69a8dc1698a46deaaa130f418b8bb8c32a95ed29
EternalYri <97452847+EternalYri@users.noreply.github.com>
2022-08-11 14:03:45 +0300

    main

---
+++ src/app/Admin/login-page/login-page.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss']
+})
+export class LoginPageComponent implements OnInit {
+
+  form!: FormGroup;
+
+  constructor() {}
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+  }
+}
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AdminModule } from './Admin/admin.module';
+import { CreatePageComponent } from './Admin/create-page/create-page.component';
+import { DashboardPageComponent } from './Admin/dashboard-page/dashboard-page.component';
+import { EditPageComponent } from './Admin/edit-page/edit-page.component';
+import { LoginPageComponent } from './Admin/login-page/login-page.component';
+import { AdminLayoutComponent } from './Admin/shared/components/admin-layout/admin-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PostPageComponent } from './post-page/post-page.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+    {path: '', redirectTo: '/', pathMatch: 'full'},
+    {path: '', component: HomePageComponent},
+    {path: 'post/:id', component: PostPageComponent}
+    ]
+  },
+  {path: 'admin', children:[
+    {
+      path: '', component: AdminLayoutComponent, children: [
+        {path: '', redirectTo: '/admin/login', pathMatch: 'full'},
+        {path: 'login', component: LoginPageComponent},
+        {path: 'dashboard', component: DashboardPageComponent},
+        {path: 'create', component: CreatePageComponent},
+        {path: 'post/:id/edit', component: EditPageComponent}
+      ]
+    }
+  ]}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
